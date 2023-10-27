fc37f63e24dbd0d4501155c3319600d338e1d96c
papai <Papai Pc@Lenovo.com>
2023-10-27 17:42:52 +0530

    code

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cart_icon from '../Assets/cart_icon.png';
 import logo from '../Assets/logo.png';
 import './Navbar.css';
 const Navbar = () => {
+  const[menu,setMenu]=useState("shop");
   return (
     <div className='navbar'>
      <div className='nav-logo'>
@@ -10,10 +11,10 @@ const Navbar = () => {
  <p>SHOPPER</p>
      </div>
      <ul className='nav-menu'>
-        <li>Shop <hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>setMenu('shop')}>Shop{menu=='shop'? <hr/> :null}</li>
+        <li onClick={()=>setMenu('men')}>Men{menu=='men'? <hr/> :null}</li>
+        <li onClick={()=>setMenu('womwn')}>Women{menu=='womwn'? <hr/> :null}</li>
+        <li onClick={()=>setMenu('kids')}>Kids {menu=='kids'? <hr/> :null}</li>
      </ul>
      <div className='nav-login-cart'>
         <button>Login</button>
