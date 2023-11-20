f99c1e160fcc5a488a6330031bbba6d12cd3a955
papai <Papai Pc@Lenovo.com>
2023-11-20 18:06:04 +0530

    code

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import dropdown from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
+import { ShopContext } from '../Context/ShopContext';
 
-const ShopCategory = () => {
+
+
+
+const ShopCategory = (props) => {
+const {ALLproduct}=useContext(ShopContext);
+
+
+
+useEffect(()=>{
+  fetch(ALLproduct).then((res)=>{
+    return res.json();
+  }).then((data)=>{
+    console.log(data);
+  })
+})
+
   return (
-    <div>
-      
+    
+    <div className='Shop_catagory'>
+      <img src={props.banner} alt='banner' />
+      <div className="shopcategory-indexSort">
+        <p>
+            <span>Showing 1-12</span> out of 36 product
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown} alt=''/>
+        </div>
+      </div>
+      <div className="shopcategory-product">
+       {
+      ALLproduct.map((item,index)=>{
+         if(props.category===item.category){
+          return <Item key={index} name={item.name} id={item.id} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+         }else{
+          return null;
+         }
+        })
+       }
+       
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ShopContextprovider from './Context/ShopContext';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <React.StrictMode>
+ 
+  <ShopContextprovider>
     <App />
-  </React.StrictMode>
-  </BrowserRouter>
+  </ShopContextprovider>
+  
+  
   
 );
 
